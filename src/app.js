0bcd6fb286db83a2d74812a3b4fd3c348904c759
Isamira360 <cardozacoboangela@gmail.com>
2026-02-25 10:51:44 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,52 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon', 'camiones', 'flor','raton', 'ciudad'];
+  let dom = ['.com', '.es', '.gov'];
+
+
+  if(dom.includes('.es')){
+    noun.forEach((vall,l)=>{
+
+      let lastLetter= vall[vall.length-1];
+      let newVal;
+
+         if ('rnds'.includes(lastLetter)) {
+
+        newVal=vall+ 'es';
+
+        noun.splice(l,1,newVal);
+
+
+
+    }
+
+    console.log(noun);
+  })
+  
+
+  pronoun.forEach((vali, i) => {
+    adj.forEach((valj, j) => {
+      noun.forEach((valk, k) => {
+        dom.forEach((vall, l) => {
+if(newVal!== vall){
+
+  console.log(noum)
+} else{
+  console.log(`${vali}${valj}${valk}${vall}`)
+}
+
+          
+          
+        })
+  })
+    })
+  })}
+
+
+  }
+
