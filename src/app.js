e371dbf71afcd02eaabb8143b1c447200a4325a6
Isamira360 <cardozacoboangela@gmail.com>
2026-02-25 19:02:44 +0000

    fusionar palabras

--- src/app.js
+++ src/app.js
@@ -5,54 +5,28 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-
-
+window.onload = function () {
+  
   let pronoun = ['the', 'our'];
   let adj = ['great', 'big'];
-  let noun = ['jogger', 'racoon', 'camiones', 'flor','raton', 'ciudad'];
+  let noun = ['jogger', 'racoon', 'camion', 'flor', 'raton', 'ciudad',];
   let dom = ['.com', '.es', '.gov'];
 
+  pronoun.forEach((vali) => {
+    adj.forEach((valj) => {
+      noun.forEach((valk) => {
+        dom.forEach((vall) => {
 
-  if(dom.includes('.es')){
-    noun.forEach((vall,l)=>{
+          let lastLetter = valk[valk.length - 1];
 
-      let lastLetter= vall[vall.length-1];
-      let newVal;
+          if (vall === '.es' && !'aeiou'.includes(lastLetter)) {
+            console.log(valk + 'es');
+            return;
+          }
 
-         if ('rnds'.includes(lastLetter)) {
-
-        newVal=vall+ 'es';
-
-        noun.splice(l,1,newVal);
-
-
-
-    }
-
-    console.log(noun);
-  })
-  
-
-  pronoun.forEach((vali, i) => {
-    adj.forEach((valj, j) => {
-      noun.forEach((valk, k) => {
-        dom.forEach((vall, l) => {
-if(newVal!== vall){
-
-  console.log(noum)
-} else{
-  console.log(`${vali}${valj}${valk}${vall}`)
-}
-
-          
-          
+          console.log(`${vali}${valj}${valk}${vall}`);
         })
-  })
+      })
     })
-  })}
-
-
-  }
-
+  })
+}
